761503574f54c74b53c429a8814b32f88b67c030
Droxl <Droxl@users.noreply.github.com>
2018-05-23 15:02:03 +0200

    Add files via upload

---
+++ Praktikumsaufgabe 03/test/ausdruck/KonstanteTest.java
@@ -0,0 +1,70 @@
+package ausdruck;
+import static org.junit.Assert.*;
+import org.junit.Test;
+/**
+ * Eine, auf JUnit 4.12 basierende, Testklasse für Tests 
+ * der Klasse Konstante.
+ * @author Droxl
+ */
+public class KonstanteTest {
+    /**
+     * Die Belegung der Konstanten.
+     */
+    private Variablenbelegung belegung;
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum ersten Mal.
+     */
+    @Test
+    public void test1GibWert() {
+        assertEquals(2, new Konstante(2).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum zweiten Mal.
+     */
+    @Test
+    public void test2GibWert() {
+        assertEquals(28, new Konstante(28).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum dritten Mal.
+     */
+    @Test
+    public void test3GibWert() {
+        assertEquals(82, new Konstante(82).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum vierten Mal.
+     */
+    @Test
+    public void test4GibWert() {
+        assertEquals(8, new Konstante(8).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum fünften Mal.
+     */
+    @Test
+    public void test5GibWert() {
+        assertEquals(5, new Konstante(5).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum sechsten Mal.
+     */
+    @Test
+    public void test6GibWert() {
+        assertEquals(6, new Konstante(6).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum siebten Mal.
+     */
+    @Test
+    public void test7GibWert() {
+        assertEquals(56, new Konstante(56).gibWert(belegung));
+    }
+    /**
+     * Testet die Methode gibWert() der Klasse Konstante zum achten Mal.
+     */
+    @Test
+    public void test8GibWert() {
+        assertEquals(65, new Konstante(65).gibWert(belegung));
+    }
+}
---
+++ Praktikumsaufgabe 03/test/ausdruck/ParserTest.java
@@ -0,0 +1,449 @@
+package ausdruck;
+import java.text.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Eine auf JUnit 4.12 basierende Testklasse
+ * für Tests mit dem Parser. Es wird davon ausgegangen,
+ * dass die angewandten Ausdrücke gültig sind.
+ * @author Droxl
+ */
+public class ParserTest {
+    /**
+     * Der Parser, welcher getestet wird.
+     */
+    private Parser parser;
+    /**
+     * Hier befindet sich das, was ausgewertet werden soll.
+     */
+    private String term = "";
+    /**
+     * Notwendig für die Wertzuweisung an eine Variable.
+     */
+    private Variablenbelegung belegung;
+    //Soll später besser werden...
+    String opTestausgabe;
+    /**
+     * Testdaten aufbauen.
+     */
+    @Before
+    public void setUp() {
+        //Neuer Parser für's „Parsen“.
+        parser = new Parser();
+        //Variablen für die Tests.
+        Variable a = new Variable("a");
+        Variable z = new Variable("z");
+        //Neue Variablenbelegung für die Werte...
+        belegung = new Variablenbelegung();
+        //a = 3
+        belegung.belege("a", 3);
+        //h = 1
+        belegung.belege("h", 1);
+        //p = 4
+        belegung.belege("p", 4);
+        //z = 6
+        belegung.belege("z", 6);
+        //x = 4
+        belegung.belege("x", 4);
+        //i = 3
+        belegung.belege("i", 3);
+        //k=4
+        belegung.belege("k", 4);
+        //j=9
+        belegung.belege("j", 9);
+        //xy=5
+        belegung.belege("xy", 5);
+        /*
+        Initialisierung des Strings, je nach Test werden ihm 
+        neue Werte übergeben.
+        */
+        term = new String();
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse1() {
+        term = "( 1 + a * 5 )";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variable für den Test.
+        Variable a = new Variable("a");
+        /**
+         * Test 1
+         * Getestet wird das „Parsen“ des Strings "( 1 + a * 5 )".
+         */
+        sollAusdruck = new Operatorausdruck(
+                new Konstante(1), '+', new Operatorausdruck(a, '*', 
+                        new Konstante(5)
+                )
+        );
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse2() {
+        term = "0";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        /**
+         * Test 2
+         * Getestet wird das Parsen des Strings "0".
+         */
+        sollAusdruck = new Konstante(0);
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse3() {
+        term = "((1 + z) - a) / 2";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variablen für den Test.
+        Variable a = new Variable("a");
+        Variable z = new Variable("z");
+        /**
+         * Test 3
+         * Getestet wird das „Parsen“ des Strings "((1 + z) - a) / 2".
+         */
+        sollAusdruck = new Operatorausdruck(new Operatorausdruck(
+                new Operatorausdruck(new Konstante(1), '+', z), 
+                '-', a), '/', new Konstante(2));
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse4() {
+        term = "5/0";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        /**
+         * Test 4
+         * Getestet wird das „Parsen“ des Strings "5/0".
+         * 
+         * Hinweis: Das Parsen führt keine Berechnung durch, daher ist hier 
+         * der String mit der Division durch 0 gültig!
+         */
+        sollAusdruck = new Operatorausdruck(new Konstante(5), '/', 
+                new Konstante(0));
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse5() {
+        term = "a+b*c";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variablen für den Test.
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        /**
+         * Test 5
+         * Getestet wird das „Parsen“ des Strings "a+b*c".
+         */
+        sollAusdruck = new Operatorausdruck(a, '+', new Operatorausdruck(
+                b, '*', c)
+        );
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse6() {
+        term = "x / ((5-a))";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variablen für den Test.
+        Variable x = new Variable("x");
+        Variable a = new Variable("a");
+        Variable b = new Variable("b");
+        Variable c = new Variable("c");
+        /**
+         * Test 6
+         * Getestet wird das „Parsen“ des Strings "x / ((5-a))".
+         */
+        sollAusdruck = new Operatorausdruck(x, '/', 
+                new Operatorausdruck(
+                        new Konstante(5), '-', a)
+        );
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse7() {
+        term = "5-2";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck = new Operatorausdruck(new Konstante(5), '-', 
+                new Konstante(2));
+        /**
+         * Test 7
+         * Getestet wird das „Parsen“ des Strings "5-2".
+         */
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse8() {
+        term = "h * (h * p)";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variablen für den Test.
+        Variable h = new Variable("h");
+        Variable p = new Variable("p");
+        /**
+         * Test 8
+         * Getestet wird das „Parsen“ des Strings "h * (h * p)".
+         */
+        sollAusdruck = new Operatorausdruck(h, '*', new Operatorausdruck(
+                h, '*', p)
+        );
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse9() {
+        term = "10";
+        /**
+         * Der jeweilige Soll-Ausdruck fuer die Tests.
+         */
+        Ausdruck sollAusdruck;
+        /**
+         * Test 9
+         * Getestet wird das „Parsen“ des Strings "10".
+         * Belegung ohne Zuordnungen und Ausdruck "10" auswerten.
+         */
+        sollAusdruck = new Konstante(10);
+        //Sollergebnis: 10
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse10() {
+        term = "7 + i";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variablen für den Test.
+        Variable i = new Variable("i");
+        /**
+         * Test 10
+         * Getestet wird das „Parsen“ des Strings "7 + i". 
+         * Belegung i=3 und Ausdruck "7 + i" auswerten.
+         */
+        sollAusdruck = new Operatorausdruck(new Konstante(7), '+', i);
+        //Sollergebnis: 10
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse11() {
+        term = "(i + 18)/2";
+        /**
+         * Der jeweilige Soll-Ausdruck für die Tests.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variablen für den Test.
+        Variable i = new Variable("i");
+        /**
+         * Test 11
+         * Getestet wird das „Parsen“ des Strings "(i + 18)/2". 
+         * Belegung i=3 und Ausdruck "7 + i" auswerten.
+         */
+        sollAusdruck = new Operatorausdruck(
+                new Operatorausdruck(i, '+', new Konstante(18)), 
+                '/', new Konstante(2));
+        //Sollergebnis: 10
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse12() {
+        term = "1 + 2 * (i + 2 * k - 1) / 2 + j";
+        /**
+         * Test 12
+         * Getestet wird das „Parsen“ des
+         * Strings: "1 + 2 * (i + 2 * k - 1) / 2 + j"
+         * mit den Belegungen: i=3, k=4, j=9.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variable i für den Test.
+        Variable i = new Variable("i");
+        //Neue Variable k für den Test.
+        Variable k = new Variable("k");
+        //Neue Variable j für den Test.
+        Variable j = new Variable("j");
+        //Aufbau des neuen Ausdrucks.
+        
+        
+        /*Realisieren:
+        OP(OP(Konstante: 1 + OP(OP(Konstante: 2 * 
+        OP(OP(i + OP(Konstante: 2 * k)) - Konstante: 1)) / Konstante: 2)) + j)
+        */
+        sollAusdruck =
+        new Operatorausdruck(
+                new Operatorausdruck(
+                        new Konstante(1), '+',
+                        new Operatorausdruck(
+                                new Operatorausdruck(
+                                        new Konstante(2), '*',
+                                        new Operatorausdruck(
+                                                new Operatorausdruck(i, '+',
+                                                        new Operatorausdruck(
+                                                                new Konstante(
+                                                                        2),
+                                                                '*', k)
+                                                ), '-', new Konstante(1)
+                                        )
+                                ), '/', new Konstante(2)
+                        )
+                ), '+', j
+        );
+        //Sollergebnis: 20
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse13() {
+        term = "a + (a - 1)";
+        /**
+         * Test 13
+         * Getestet wird das „Parsen“ des
+         * Strings: "a + (a - 1)"
+         * mit der Belegung: a=3.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variable i für den Test.
+        Variable a = new Variable("a");
+        //Aufbau des neuen Ausdrucks.
+        sollAusdruck = new Operatorausdruck(a, '+',
+                new Operatorausdruck(a, '-', new Konstante(1)
+                )
+        );
+        //Sollergebnis: 5
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+    //Fällt erst mal raus: a * g(f(2), f(a - 1) + 2)
+    /**
+     * Testet die Methode {@link Parser#parse(java.lang.String)}.
+     */
+    @Test
+    public void testParse14() {
+        term = "(a + 1) * xy - 37";
+        /**
+         * Test 14
+         * Getestet wird das „Parsen“ des
+         * Strings: "(a + 1) * xy - 37"
+         * mit der Belegung: a=3.
+         */
+        Ausdruck sollAusdruck;
+        //Neue Variable i für den Test.
+        Variable a = new Variable("a");
+        Variable xy = new Variable("xy");
+        Konstante konst1 = new Konstante(37);
+        //Aufbau des neuen Ausdrucks.
+        sollAusdruck = new Operatorausdruck(
+                new Operatorausdruck(
+                        new Operatorausdruck(a, '+', new Konstante(1)), 
+                        '*', xy), '-', konst1);
+        //Sollergebnis: -17
+        try {
+            assertEquals(sollAusdruck, parser.parse(term));
+        } catch (ParseException ex) {
+            fail(ex.getMessage());
+        }
+    }
+}
